66559a5a5ea87d395195d69433468f4f3aa93a57
AmielCo <amiel.coronel@gmail.com>
2022-06-27 12:11:31 +0300

    solidity WIP

--- contracts/Hero.sol
+++ contracts/Hero.sol
@@ -22,11 +22,31 @@ contract Hero {
         return addressToHeroes[msg.sender];
     }
 
+    function getStrenght(uint256 hero) public pure returns (uint256) {
+        return (hero >> 2) & 0x1F;
+    }
+
+    function getHealth(uint256 hero) public pure returns (uint256) {
+        return (hero >> 7) & 0x1F;
+    }
+
+    function getDex(uint256 hero) public pure returns (uint256) {
+        return (hero >> 12) & 0x1F;
+    }
+
+    function getIntellect(uint256 hero) public pure returns (uint256) {
+        return (hero >> 17) & 0x1F;
+    }
+
+    function getMagic(uint256 hero) public pure returns (uint256) {
+        return (hero >> 22) & 0x1F;
+    }
+
     function createHero(Class class) public payable {
         require(msg.value >= 0.05 ether, "please send more money !");
         uint256[] memory stats = new uint256[](5);
-        stats[0] = 2;
-        stats[1] = 7;
+        stats[0] = 2; 
+        stats[1] = 7; 
         stats[2] = 12;
         stats[3] = 17;
         stats[4] = 22;
@@ -36,7 +56,7 @@ contract Hero {
         do {
             uint256 pos = generateRandom() % len;
             uint256 value = (generateRandom() % (13 + len)) + 1;
-            hero |= value << stats[pos];
+            hero |= value << stats[pos]; 
             len--;
             stats[pos] = stats[len];
         } while (len > 0);
